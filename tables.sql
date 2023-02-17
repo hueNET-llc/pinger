fabd36c6eba2f02b44a3cad59a7b70e94f041769
Rafael <mail@huenet.net>
2023-02-17 11:16:19 -0500

    Simplify pinger, fix some stuff
    Remove useless bloat
    
    Add platform-specific workflows
    
    Change table partitioning to YYYYMM

--- tables.sql
+++ tables.sql
@@ -3,41 +3,21 @@
 -- You may have to modify them to work in your setup
 
 CREATE TABLE pinger (
-    type LowCardinality(String),
-    host_country LowCardinality(Nullable(String)),
-    host_state LowCardinality(Nullable(String)),
-    host_city LowCardinality(Nullable(String)),
-    host_name LowCardinality(String),
-    host_asn Nullable(int),
-    target_name LowCardinality(String),
-    target_country LowCardinality(Nullable(String)),
-    target_state LowCardinality(Nullable(String)),
-    target_city LowCardinality(Nullable(String)),
-    target_asn Nullable(int),
-    target_ip LowCardinality(String),
+    name LowCardinality(String),
+    ip LowCardinality(String),
     avg_ms Nullable(Float),
     max_ms Nullable(Float),
     min_ms Nullable(Float),
     loss_percent Float,
     time DateTime DEFAULT now()
-) ENGINE = MergeTree() PARTITION BY toDate(time) ORDER BY (host_name, target_name, time) PRIMARY KEY (host_name, target_name, time);
+) ENGINE = MergeTree() PARTITION BY toYYYYMM(time) ORDER BY (name, time) PRIMARY KEY (name, time);
 
 CREATE TABLE pinger_buffer (
-    type LowCardinality(String),
-    host_country LowCardinality(Nullable(String)),
-    host_state LowCardinality(Nullable(String)),
-    host_city LowCardinality(Nullable(String)),
-    host_name LowCardinality(String),
-    host_asn Nullable(int),
-    target_name LowCardinality(String),
-    target_country LowCardinality(Nullable(String)),
-    target_state LowCardinality(Nullable(String)),
-    target_city LowCardinality(Nullable(String)),
-    target_asn Nullable(int),
-    target_ip LowCardinality(String),
+    name LowCardinality(String),
+    ip LowCardinality(String),
     avg_ms Nullable(Float),
     max_ms Nullable(Float),
     min_ms Nullable(Float),
     loss_percent Float,
     time DateTime DEFAULT now()
-) ENGINE = Buffer(homelab, pinger, 1, 10, 10, 10, 100, 10000, 10000);
+) ENGINE = Buffer(homelab, pinger_targets, 1, 10, 10, 10, 100, 10000, 10000);
